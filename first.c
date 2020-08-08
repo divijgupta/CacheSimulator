9618e033248b005c6018a3d43040cc9ac9758bb5
Divij Gupta <divijgupta@gmail.com>
2020-08-08 16:22:36 -0400

    Basic format

--- first.c
+++ first.c
@@ -3,7 +3,38 @@
 #include <stdio.h>
 #include <string.h>
 
-//ADD COMPILATION FLAGS
+#define MEMORYSIZE 48
+
+//global values that keep track of cache and memory info
+int cacheHit = 0;
+int cacheMiss = 0;
+int memoryRead = 0;
+int memoryWrite = 0;
+
+//will perform the read operation
+void read(char* address) {
+
+	printf("%s\n", address);
+
+}
+
+//will perform the write operation
+void write(char* address) {
+	
+	printf("%s\n", address);
+
+}
+
+//if not found in read or write, will add to cache
+void addToCache() {
+
+}
+
+//looks for memory address within the cache
+void searchInCache() {
+
+}
+
 int main(int argc, char **argv)
 {
 
@@ -23,37 +54,89 @@ int main(int argc, char **argv)
 	char* cachePolicy = argv[3];
 	char* associativity = argv[4];
 	char* data = argv[5];
-	//open the file
-	FILE *file = fopen(data, "r");
 
-	//will be updated as we move along the file
-	char* accessType = "";
-	char* memoryAddress;
-	//read the file for its contents
-	while (strcmp(accessType, "#eof") != 0) {
+	//calculate the number of bits for offset
+	int offsetBits = log(blockSize) / log(2);
+	int numSets = 0;
+	int setBits = 0;
+	int numLines = 0;
+	int tagBits = 0;
 
-		//read the first value
-		fscanf(file, "%s", accessType);
+//check associativity type to get cache size
+	if (strcmp(associativity, "direct") == 0){
 
-		//check if the value is #eof
-		if (strcmp(accessType, "#eof") != 0) {
-			break;
-		}
+		numSets = cacheSize / blockSize;
+		setBits = log(numSets) / log(2);
+		numLines = numSets;
 
-		//read the second value
-		fscanf(file, "%s\n", memoryAddress);
+	} else if (strcmp(associativity, "assoc") == 0) {
 
-		printf("%s\t",accessType);
-		printf("%s\n",memoryAddress);
+		numSets = 1;
+		setBits = 0;
+		numLines = cacheSize / blockSize;
+
+	} else {
+
+		//gets the number of ways the set is associative
+		char* num = strtok(associativity, ":");
+		num = strtok(NULL, "");
+		int numWays = atoi(num);
+
+		printf( "%d\n",numWays);
+
+		numLines = cacheSize / blockSize;
+		numSets = numLines / numWays;
 
 	}
 
-
+    tagBits = MEMORYSIZE - offsetBits - setBits;
 	
 
-	//close the file
-	fclose(file);
+    //open the file
+    FILE *file = fopen(data, "r");
 
+    //will be updated as we move along the file
+    char accessType[10] = "";
+    char memoryAddress[15] = "";
+
+    //read the file for its contents
+    while (fscanf(file, "%s%s\n", accessType, memoryAddress) != EOF) {
+
+		//makes sure the end of the file has not been reached yet
+		if (strcmp(accessType, "#eof") != 0){
+        
+        //check if it is a read or write operation
+		if (strcmp(accessType, "R") == 0) {
+
+			read(memoryAddress);
+
+		} else {
+
+		    write(memoryAddress);
+
+		}
+
+		//checks the cache policy
+		if (strcmp(cachePolicy, "fifo")) {
+
+		}
+
+		}
+
+    }
+
+	//print out output
+	printf("%s%d\n", "Memory reads: ", memoryRead);
+	printf("%s%d\n", "Memory writes: ", memoryWrite);
+	printf("%s%d\n", "Cache hits: ", cacheHit);
+	printf("%s%d\n", "Cache misses: ", cacheMiss);
+
+
+
+    
+
+    //close the file
+    fclose(file);
 
 
 	}
